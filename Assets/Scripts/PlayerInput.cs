ca3833b04fb604c518368c60905bdd0ba6e16991
Oleg Trubitsin <arakir88@gmail.com>
2020-05-28 12:24:34 +0300

    prevent unintended ascending

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -36,11 +36,11 @@ public class PlayerInput : MonoBehaviour {
     }
 
     private void ApplyMovement() {
-        if (movement.ActionsIsDisabled()) return;
+        if (movement.IsAllActionsDisabled()) return;
         movement.Move(xInput * Time.fixedDeltaTime);
     }
     private void ApplyActions() {
-        if (movement.ActionsIsDisabled()) return;
+        if (movement.IsAllActionsDisabled()) return;
         if (Input.GetKeyDown(resetLevel)) SceneManager.LoadScene(0);
         
         if (Input.GetButton("Walk")) {
@@ -57,7 +57,7 @@ public class PlayerInput : MonoBehaviour {
         if (Input.GetButtonDown("Dodge")) {
             movement.Dodge();
         }
-
+        
         if (Input.GetButton("Crouch")) {
             movement.Crouch();
         } else movement.UnCrouch();
